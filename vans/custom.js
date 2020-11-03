c5b1f114607c2c3b22e2bb86d62b3b2b5d8a0df4
gytjr <gytjr321@naver.com>
2020-11-03 18:23:16 +0900

    vans 애니매이션 슬라이더 추가

--- vans/custom.js
+++ vans/custom.js
@@ -1,6 +1,5 @@
+$(document).ready(function () {
 
-$(document).ready(function(){
-    
     let didScroll;
     var lastScrollTop = 0;
     var delta = 5; // 동작의 구현이 시작되는 위치
@@ -33,13 +32,22 @@ $(document).ready(function(){
         lastScrollTop = st;
     }
 
-    $(".header>.logo").on("click", function(){
-        $('html, body').animate({scrollTop : 0}, 400);
+    $('.visual').slick({
+        dots: true,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    })
+
+    $(".header>.logo").on("click", function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 400);
     })
 
     $con1btn = $(".con1btn>li");
 
-    $(".con1btn>li").on("click", function(e){
+    $(".con1btn>li").on("click", function (e) {
         e.preventDefault();
 
         i = $(this).index();
@@ -52,29 +60,92 @@ $(document).ready(function(){
 
     })
 
-    $(".iconleft").on("click", function(){
-        $(".simg-t").stop().animate({"margin-left" : "0px"},500,function(){
+    $(".iconleft").on("click", function () {
+        $(".simg-t").stop().animate({
+            "margin-left": "0px"
+        }, 500, function () {
             $(".simg-t>div").last().prependTo(".simg-t");
-            $(".simg-t").css({"margin-left" : "-34%"})
+            $(".simg-t").css({
+                "margin-left": "-34%"
+            })
         })
     })
 
-    $(".iconright").on("click", function(){
-        $(".simg-t").stop().animate({"margin-left" : "-68%"},500,function(){
+    $(".iconright").on("click", function () {
+        $(".simg-t").stop().animate({
+            "margin-left": "-68%"
+        }, 500, function () {
             $(".simg-t>div").first().appendTo(".simg-t");
-            $(".simg-t").css({"margin-left" : "-34%"})
+            $(".simg-t").css({
+                "margin-left": "-34%"
+            })
         })
     })
 
-    $(".cart").on("click", function(){
+    $(".cart").on("click", function () {
         alert('장바구니에 추가되었습니다.');
     })
-    $(".like").on("click", function(){
-        if($(this).hasClass('on') == false){
+    $(".like").on("click", function () {
+        if ($(this).hasClass('on') == false) {
             $(this).addClass('on');
         } else {
             $(this).removeClass('on');
         }
     })
+
+    /* 발견되면 활성화시키는 라이브러리 시작 */
+    function ActiveOnVisible__init() {
+        $(window).resize(ActiveOnVisible__initOffset);
+        ActiveOnVisible__initOffset();
+
+        $(window).scroll(ActiveOnVisible__checkAndActive);
+        ActiveOnVisible__checkAndActive();
+    }
+
+    function ActiveOnVisible__initOffset() {
+        $('.active-on-visible').each(function (index, node) {
+            var $node = $(node);
+
+            var offsetTop = $node.offset().top;
+            $node.attr('data-active-on-visible-offsetTop', offsetTop);
+
+            if (!$node.attr('data-active-on-visible-diff-y')) {
+                $node.attr('data-active-on-visible-diff-y', '0');
+            }
+
+            if (!$node.attr('data-active-on-visible-delay')) {
+                $node.attr('data-active-on-visible-delay', '0');
+            }
+        });
+
+        ActiveOnVisible__checkAndActive();
+    }
+
+    function ActiveOnVisible__checkAndActive() {
+        $('.active-on-visible:not(.actived)').each(function (index, node) {
+            var $node = $(node);
+
+            var offsetTop = $node.attr('data-active-on-visible-offsetTop') * 1;
+            var diffY = parseInt($node.attr('data-active-on-visible-diff-y'));
+            var delay = parseInt($node.attr('data-active-on-visible-delay'));
+
+            var callbackFuncName = $node.attr('data-active-on-visible-callback-func-name');
+
+            if ($(window).scrollTop() + $(window).height() + diffY > offsetTop) {
+                $node.addClass('actived');
+
+                setTimeout(function () {
+                    $node.addClass('active');
+                    if (window[callbackFuncName]) {
+                        window[callbackFuncName]($node);
+                    }
+                }, delay);
+            }
+        });
+    }
+
+    ActiveOnVisible__init();
+
+    /* 발견되면 활성화시키는 라이브러리 끝 */
 
 })
